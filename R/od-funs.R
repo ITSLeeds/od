55d6ff64513020728e49cd8b4665d45fa5bd9808
Robin Lovelace <rob00x@gmail.com>
2020-02-08 22:30:06 +0000

    Working od_o_sf = 2 times faster than od2line()

--- R/od-funs.R
+++ R/od-funs.R
@@ -1,73 +1,73 @@
+#' Convert origin-destination coordinates into desire lines
+#'
+#' @param odc A data frame or matrix representing the coordinates
+#' of origin-destination data. The first two columns represent the
+#' coordinates of the origin (typically longitude and latitude) points;
+#' the third and fourth columns represent the coordinates of the destination
+#' (in the same CRS). Each row represents travel from origin to destination.
+#' @param crs A number representing the coordinate reference system
+#' of the result, 4326 by default.
 #' @examples
-#' x = od_data_leeds
-#' class(x)
-#' x_od = od(x)
-#' class(x_od)
-od = function(x) {
-  class(x) = c("od", class(x))
-  return(x)
+#' desire_lines = od_to_sf(od_data_leeds, od_data_zones)
+#' plot(desire_lines)
+#'
+#' desire_lines2 = stplanr::od2line(od_data_leeds, od_data_zones)
+#' plot(desire_lines2[-c(1:2)])
+#' bench::mark(check = FALSE,
+#'   desire_lines = od_to_sf(od_data_leeds, od_data_zones)
+#'   desire_lines2 = stplanr::od2line(od_data_leeds, od_data_zones)
+#' )
+od_to_sf = function(odf, zones, destinations = NULL) {
+  odc = od_coordinates(x = odf, z = zones)
+  odc_to_sf(odc, df = odf[-c(1:2)])
 }
 
+#' Convert od coordinates to sf object
 #' @examples
-#' x = od_data_leeds
-#' p = sf::st_centroid(od::od_data_zones)
-od_coordinates_from_points = function(x, p, p_destinations = NULL) {
-  o_code = x[[1]]
-  d_code = x[[2]]
-  p_code = p[[1]]
-  stopifnot(all(o_code %in% p_code)) # todo: add error message
-  o_matching_p = match(o_code, p_code)
-  d_matching_p = match(d_code, p_code)
-  p_coordinates = sf::st_coordinates(p)
-  o_coords = p_coordinates[o_matching_p, ]
-  d_coords = p_coordinates[d_matching_p, ]
-  od_coordinates = cbind(o_coords, d_coords)
-  colnames(od_coordinates) = c("ox", "oy", "dx", "dy")
-  od_coordinates
-}
-
-#' @examples
-#' od_coordinates(od_data_leeds, od::od_data_zones)
-od_coordinates = function(x, z) {
-  geometry_contains_polygons = geometry_contains_polygons(z)
-  if(geometry_contains_polygons) { suppressWarnings({
-      sf::st_geometry(z) = sf::st_centroid(sf::st_geometry(z))
-  })}
-  od_coordinates_from_points(x, z)
-}
-
-geometry_contains_polygons = function(z) {
-  grepl(pattern = "POLY", unique(sf::st_geometry_type(z)))
-}
-
-#' @examples
-#' odm = od_coordinates(od_data_leeds, od_data_zones)
-#' odm_id = od:::od_coordinates_ids(odm)
-#' odl = od_coordinates_to_linstring(odm_id)
+#' odc = od_coordinates(od_data_leeds, od_data_zones)
+#' odl = od_coordinates_to_sfc(odc)
 #' class(odl)
 #' plot(odl)
 #'
+#' odc_to_sf(odc, df = od_data_leeds)
+#' odsf = odc_to_sf()
+#'
 #' # testing the result
+#' odlsf = od_coordinates_to_sfc(odc, "sf")
 #' odsf_stplanr = stplanr::od2line(od_data_leeds, od_data_zones)
 #' odl_stplanr = sf::st_geometry(odsf_stplanr)
 #' odl_stplanr[1]
 #' odl[1]
+#' odlsf[1]
 #' bench::mark(check = FALSE,
-#' od_coordinates_to_linstring(odm_id),
-#' stplanr::od2line(od_data_leeds, od_data_zones)
+#'   od_coordinates_to_sfc(odc),
+#'   od_coordinates_to_sfc(odc, package = "sf"),
+#'   stplanr::od2line(od_data_leeds, od_data_zones)
 #' )
 #' # system.time({odl_stplanr = stplanr::od2line(od_data_leeds, od_data_zones)})
 #' # plot(sf::st_geometry(odl_stplanr))
-od_coordinates_to_linstring = function(odm) {
-  sfheaders::sfc_linestring(obj = odm_id, x = "x", y = "y", linestring_id = "id")
+odc_to_sf = function(odc, df, package = "sfheaders", crs = 4326) {
+  sfc = od_coordinates_to_sfc(odc, package = package, crs = crs)
+  sf::st_sf(df, geometry = sfc)
+}
+od_coordinates_to_sfc = function(odc, package = "sfheaders", crs = 4326) {
+  if(package == "sfheaders") {
+    odc_id = od_coordinates_ids(odc)
+    sfheaders::sfc_linestring(obj = odc_id, x = "x", y = "y", linestring_id = "id")
+  } else {
+    linestring_list = lapply(seq(nrow(odc)), function(i) {
+      sf::st_linestring(rbind(odc[i, 1:2], odc[i, 3:4]))
+    })
+    sf::st_sfc(linestring_list, crs = crs)
+    }
 }
 
-od_coordinates_ids = function(odm) {
-  res = data.frame(id = rep(1:nrow(odm), each = 2), x = NA, y = NA)
+od_coordinates_ids = function(odc) {
+  res = data.frame(id = rep(1:nrow(odc), each = 2), x = NA, y = NA)
   ids_odd = seq(1, nrow(res), by = 2)
   ids_even = ids_odd + 1
-  res[ids_odd, c("x", "y")] = odm[, 1:2]
-  res[ids_even, c("x", "y")] = odm[, 3:4]
+  res[ids_odd, c("x", "y")] = odc[, 1:2]
+  res[ids_even, c("x", "y")] = odc[, 3:4]
   res
 }
 
@@ -167,225 +167,8 @@ od_coords <- function(from = NULL, to = NULL, l = NULL) {
 
 }
 
-#' Convert origin-destination coordinates into desire lines
-#'
-#' @param odc A data frame or matrix representing the coordinates
-#' of origin-destination data. The first two columns represent the
-#' coordinates of the origin (typically longitude and latitude) points;
-#' the third and fourth columns represent the coordinates of the destination
-#' (in the same CRS). Each row represents travel from origin to destination.
-#' @param crs A number representing the coordinate reference system
-#' of the result, 4326 by default.
-#' @param remove_duplicates Should rows with duplicated rows be removed? `TRUE` by default.
-#' @family od
-#' @export
-#' @examples
-#' odf <- od_coords(l = flowlines_sf)
-#' odlines <- od_coords2line(odf)
-#' odlines <- od_coords2line(odf, crs = 4326)
-#' plot(odlines)
-#' x_coords = 1:3
-#' n = 50
-#' d = data.frame(lapply(1:4, function(x) sample(x_coords, n, replace = TRUE)))
-#' names(d) = c("fx", "fy", "tx", "ty")
-#' l = od_coords2line(d)
-#' plot(l)
-#' nrow(l)
-#' l_with_duplicates = od_coords2line(d, remove_duplicates = FALSE)
-#' plot(l_with_duplicates)
-#' nrow(l_with_duplicates)
-od_coords2line <- function(odc, crs = 4326, remove_duplicates = TRUE) {
-  odc_unique <- odc[!duplicated(odc[, 1:4]), ]
-  if(nrow(odc_unique) < nrow(odc) && remove_duplicates) {
-    message("Duplicate OD pairs identified, removing ", nrow(odc) - nrow(odc_unique), " rows")
-    odc <- odc_unique
-    odc_unique$n = dplyr::group_size(dplyr::group_by_all(as.data.frame(odc[, 1:4])))
-  }
-  odm <- as.matrix(odc)
-  linestring_list <- lapply(seq(nrow(odm)), function(i) {
-    sf::st_linestring(rbind(odm[i, 1:2], odm[i, 3:4]))
-  })
-  sf::st_sf(odc, geometry = sf::st_sfc(linestring_list, crs = crs))
-}
-#' Convert origin-destination data to spatial lines
-#'
-#' Origin-destination ('OD') flow data is often provided
-#' in the form of 1 line per flow with zone codes of origin and destination
-#' centroids. This can be tricky to plot and link-up with geographical data.
-#' This function makes the task easier.
-#'
-#' @details
-#' Origin-destination (OD) data is often provided
-#' in the form of 1 line per OD pair, with zone codes of the trip origin in the first
-#' column and the zone codes of the destination in the second column
-#' (see the [`vignette("stplanr-od")`](https://docs.ropensci.org/stplanr/articles/stplanr-od.html)) for details.
-#' `od2line()` creates a spatial (linestring) object representing movement from the origin
-#' to the destination for each OD pair.
-#' It takes data frame containing
-#' origin and destination cones (`flow`) that match the first column in a
-#' a spatial (polygon or point) object (`zones`).
-#'
-#' @param flow A data frame representing origin-destination data.
-#'  The first two columns of this data frame should correspond
-#' to the first column of the data in the zones. Thus in [cents()],
-#' the first column is geo_code. This corresponds to the first two columns
-#' of [flow()].
-#' @param zones A spatial object representing origins (and destinations
-#' if no separate destinations object is provided) of travel.
-#' @param destinations A spatial object
-#' representing destinations of travel flows.
-#' @param zone_code Name of the variable in `zones` containing the ids of the zone.
-#' By default this is the first column names in the zones.
-#' @param origin_code Name of the variable in `flow` containing the ids of the zone of origin.
-#' By default this is the first column name in the flow input dataset.
-#' @param dest_code Name of the variable in `flow` containing the ids of the zone of destination.
-#' By default this is the second column name in the flow input dataset or the first column name in the
-#' destinations if that is set.
-#' @param zone_code_d Name of the variable in `destinations` containing the ids of the zone.
-#' By default this is the first column names in the destinations.
-#' @param silent TRUE by default, setting it to TRUE will show you the matching columns
-#' @family od
-#' @export
-#' @examples
-#' l <- od2line(flow = flow, zones = cents)
-#' plot(cents)
-#' plot(l, lwd = l$All / mean(l$All), add = TRUE)
-#' # When destinations are different
-#' head(flow_dests[1:5]) # check data
-#' head(destinations[1:5])
-#' flowlines_dests <- od2line(flow_dests, cents, destinations = destinations)
-#' plot(flowlines_dests)
-#' l <- od2line(flow, zones_sf)
-#' plot(l["All"], lwd = l$All/mean(l$All))
-#' @name od2line
-NULL
-
-#' @rdname od2line
-#' @export
-od2line <- function(flow, zones, destinations = NULL,
-                    zone_code = names(zones)[1],
-                    origin_code = names(flow)[1],
-                    dest_code = names(flow)[2],
-                    zone_code_d = NA, silent = FALSE) {
-  UseMethod("od2line", object = zones)
-}
-#' @export
-od2line.sf <- function(flow, zones, destinations = NULL,
-                       zone_code = names(zones)[1],
-                       origin_code = names(flow)[1],
-                       dest_code = names(flow)[2],
-                       zone_code_d = NA, silent = TRUE) {
-  if (grepl(pattern = "POLYGON", x = unique(sf::st_geometry_type(zones)))) {
-    message("Creating centroids representing desire line start and end points.")
-    zones <- sf::st_centroid(zones)
-  }
-
-  coords_o <- sf::st_coordinates(zones)[, 1:2]
-  origin_points <- coords_o[match(flow[[origin_code]], zones[[zone_code]]), ]
-
-  if (is.null(destinations)) {
-    if (!silent) {
-      message(paste(
-        "Matching", zone_code, "in the zones to", origin_code, "and", dest_code,
-        "for origins and destinations respectively"
-      ))
-    }
-
-    dest_points <- coords_o[match(flow[[dest_code]], zones[[zone_code]]), ]
-  } else {
-    dest_points <- coords_o[match(flow[[dest_code]], destinations[[zone_code_d]]), ]
-  }
-
-  odm = cbind(origin_points, dest_points)
-
-  odsfc <- od_coords2line(odm, crs = sf::st_crs(zones), remove_duplicates = FALSE)
-  sf::st_sf(flow, geometry = odsfc$geometry)
-
-}
 points2line <- function(p) {
   UseMethod("points2line")
-}
-#' @export
-points2line.sf <- function(p) {
-  points2flow(p = p)
-}
-#' @export
-points2line.Spatial <- function(p) {
-  if (is(p, "SpatialPoints")) {
-    p_proj <- sp::proj4string(p)
-    p <- sp::coordinates(p)
-  } else {
-    p_proj <- NA
-  }
-  l <- points2line(p)
-  raster::crs(l) <- p_proj
-  l
-}
-#' @export
-points2line.matrix <- function(p) {
-  l <- raster::spLines(p)
-  l
-}
-#' Summary statistics of trips originating from zones in OD data
-#'
-#' This function takes a data frame of OD data and
-#' returns a data frame reporting summary statistics for each unique zone of origin.
-#'
-#' It has some default settings: the default summary statistic is `sum()` and the
-#' first column in the OD data is assumed to represent the zone of origin.
-#' By default, if `attrib` is not set, it summarises all numeric columns.
-#'
-#' @inheritParams od2odf
-#' @inheritParams overline
-#' @param FUN A function to summarise OD data by
-#' @param col The column that the OD dataset is grouped by
-#' (1 by default, the first column usually represents the origin)
-#' @param ... Additional arguments passed to `FUN`
-#' @family od
-#' @export
-#' @examples
-#' od_aggregate_from(flow)
-od_aggregate_from <- function(flow, attrib = NULL, FUN = sum, ..., col = 1) {
-  if(is.character(attrib)) {
-    attrib_lgl <- grepl(pattern = attrib, x = names(flow))
-    if(sum(attrib_lgl) == 0){
-      stop("No columns match the attribute ", attrib)
-    }
-    attrib = which(attrib_lgl)
-  }
-  if(!is.null(attrib)) {
-    flow <- flow[attrib]
-  }
-  flow_grouped <- dplyr::group_by_at(flow, col)
-  dplyr::summarise_if(flow_grouped, is.numeric, .funs = FUN, ...)
-}
-#' Summary statistics of trips arriving at destination zones in OD data
-#'
-#' This function takes a data frame of OD data and
-#' returns a data frame reporting summary statistics for each unique zone of destination.
-#'
-#' It has some default settings: it assumes the destination ID column is the 2nd
-#' and the default summary statistic is `sum()`.
-#' By default, if `attrib` is not set, it summarises all numeric columns.
-#'
-#' @inheritParams od_aggregate_from
-#' @family od
-#' @export
-#' @examples
-#' od_aggregate_to(flow)
-od_aggregate_to <- function(flow, attrib = NULL, FUN = sum, ..., col = 2) {
-  if(is.character(attrib)) {
-    attrib_lgl <- grepl(pattern = attrib, x = names(flow))
-    if(sum(attrib_lgl) == 0){
-      stop("No columns match the attribute ", attrib)
-    }
-    attrib = which(attrib_lgl)
-  }
-  if(!is.null(attrib)) {
-    flow <- flow[attrib]
-  }
-  flow_grouped <- dplyr::group_by_at(flow, col)
-  dplyr::summarise_if(flow_grouped, is.numeric, .funs = FUN, ...)
 }
 
 #' Convert origin-destination data from long to wide format
@@ -444,3 +227,37 @@ odmatrix_to_od <- function(odmatrix) {
   od <- stats::na.omit(od)
   od[order(paste0(od$orig, od$dest)), ]
 }
+
+
+#' @examples
+#' x = od_data_leeds
+#' p = sf::st_centroid(od::od_data_zones)
+od_coordinates_from_points = function(x, p, p_destinations = NULL) {
+  o_code = x[[1]]
+  d_code = x[[2]]
+  p_code = p[[1]]
+  stopifnot(all(o_code %in% p_code)) # todo: add error message
+  o_matching_p = match(o_code, p_code)
+  d_matching_p = match(d_code, p_code)
+  p_coordinates = sf::st_coordinates(p)
+  o_coords = p_coordinates[o_matching_p, ]
+  d_coords = p_coordinates[d_matching_p, ]
+  od_coordinates = cbind(o_coords, d_coords)
+  colnames(od_coordinates) = c("ox", "oy", "dx", "dy")
+  od_coordinates
+}
+
+#' @examples
+#' od_coordinates(od_data_leeds, od::od_data_zones)
+od_coordinates = function(x, z) {
+  geometry_contains_polygons = geometry_contains_polygons(z)
+  if(geometry_contains_polygons) { suppressWarnings({
+    sf::st_geometry(z) = sf::st_centroid(sf::st_geometry(z))
+  })}
+  od_coordinates_from_points(x, z)
+}
+
+geometry_contains_polygons = function(z) {
+  grepl(pattern = "POLY", unique(sf::st_geometry_type(z)))
+}
+
