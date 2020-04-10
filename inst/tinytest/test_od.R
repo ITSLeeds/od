d442beb71bee677689fdb5a1cc23269aea831649
Robin Lovelace <rob00x@gmail.com>
2020-04-10 15:59:29 +0100

    Update tests

--- inst/tinytest/test_od.R
+++ inst/tinytest/test_od.R
@@ -6,10 +6,18 @@ expect_equal(1 + 1, 2)
 # Test that OD operations with NA coordinates generate warnings:
 o = od_data_df
 z = od_data_zones_min
-tinytest::expect_true(is(od_to_sf(o, z), "sf"))
+expect_true(is(od_to_sf(o, z), "sf"))
 
 o$geo_code1[1] = "444"
 expect_error(od_to_sf(o, z))
 o = od_data_df
 o$geo_code2[1] = "444"
 expect_error(od_to_sf(o, z))
+
+o = od_data_df
+# test crss
+expect_true({
+  z_projected = sf::st_transform(z, 27700)
+  is((l = od_to_sfc(o, z_projected)), "sfc")
+})
+expect_true(length(l) == nrow(x))
