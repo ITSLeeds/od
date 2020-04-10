cbd801aafd9b48743b75b9d83b14e04bab72df60
Robin Lovelace <rob00x@gmail.com>
2020-04-10 15:50:04 +0100

    Add working tests

--- inst/tinytest/test_od.R
+++ inst/tinytest/test_od.R
@@ -6,14 +6,10 @@ expect_equal(1 + 1, 2)
 # Test that OD operations with NA coordinates generate warnings:
 o = od_data_df
 z = od_data_zones_min
-od_to_sf(o, z)
+tinytest::expect_true(is(od_to_sf(o, z), "sf"))
 
-o$geo_code2[1] = "444"
-od_to_sf(o, z) # should error
-sf::st_geometry(od_to_sf(o, z))
-
-# formal test with tinytest
-library(tinytest)
-expect_error(od_to_sf(o, z))
 o$geo_code1[1] = "444"
+expect_error(od_to_sf(o, z))
+o = od_data_df
+o$geo_code2[1] = "444"
 expect_error(od_to_sf(o, z))
