fb7f38807d1887b09321700be7b84222a3840cfc
Robin Lovelace <rob00x@gmail.com>
2021-03-02 13:01:07 +0000

    Add code for #27

--- data-raw/test-disag.R
+++ data-raw/test-disag.R
@@ -8,12 +8,15 @@ desire_lines_many = sf::read_sf(u)
 
 zones_msoa_national = pct::get_pct(layer = "z", geography = "msoa", national = TRUE)
 centroids_lsoa_national = pct::get_pct(layer = "c", national = TRUE)
-zones_lsoa_national = pct::get_pct(layer = "z", national = TRUE)
+zones_lsoa_national = pct::get_pct(layer = "z", national = TRUE, geography = "lsoa")
 zones_many = zones_msoa_national %>% filter(geo_code %in% desire_lines_many$geo_code2)
 centroids_lsoa_many = centroids_lsoa_national[zones_many, ]
 zones_lsoa_many = zones_lsoa_national %>% filter(geo_code %in% centroids_lsoa_many$geo_code)
 
-qtm(zones_lsoa_many) +
-  qtm(desire_lines_many)
+qtm(zones_many, borders.lwd = 3) +
+  qtm(desire_lines_many) +
+  tm_shape(zones_lsoa_many) + tm_borders("red", lty = 3)
 
-desire_lines_many = od::od_disaggregate(od = desire_lines_many %>% select(geo_code1:all_base), z = zones_many, subzones = zones_lsoa_many)
+
+
+desire_lines_many = od::od_disaggregate(od = desire_lines_many %>% select(geo_code1, geo_code2, all_base), z = zones_many, subzones = zones_lsoa_many)
